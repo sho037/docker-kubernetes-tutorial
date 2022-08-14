89fffb9e51b389089ab9403da22a02389eff50cf
mu-ruU1 <83493474+mu-ruU1@users.noreply.github.com>
2022-08-14 23:03:36 +0900

    コメントを削除

--- sample/echo-pods/src/entrypoint.sh
+++ sample/echo-pods/src/entrypoint.sh
@@ -3,10 +3,8 @@ set -e
 
 key='COLOR="#'
 
-# get 4 byte random numbers in base 16
 random=`od -An -N4 -tx < /dev/urandom`
 
-# cut out subtext from 1 to 6
 color=`echo $random | cut -c 1-6`
 
 echo "$key$color\"" > .env
